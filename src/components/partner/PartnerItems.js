94857b4551440a9a6217de99651c4dbf8596817e
Kim Ongchangco <155955124+ongchangco@users.noreply.github.com>
2025-05-31 13:00:06 +0800

    Text Content for Partnerships

--- src/components/partner/PartnerItems.js
+++ src/components/partner/PartnerItems.js
@@ -4,20 +4,20 @@ export const PartnerItems = [
     name: "MetaMask",
     imgURL: "metamask",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+      "A leading crypto wallet and Web3 gateway that enables secure blockchain transactions and seamless interaction with decentralized applications, making it an ideal partner for powering transparent and user-controlled energy market solutions on the blockchain.",
   },
   {
     id: "2",
     name: "Ethereum",
     imgURL: "ethereum",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+      "Decentralized blockchain platform that enables the creation of smart contracts and decentralized applications (dApps), providing a secure and transparent foundation for powering innovative energy market solutions and peer-to-peer transactions.",
   },
   {
     id: "3",
     name: "Polygon",
     imgURL: "polygon",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+      "Scalable, Ethereum-compatible blockchain network that offers fast, low-cost transactions, providing a robust and efficient foundation for developing decentralized and sustainable energy market systems.",
   },
 ];
